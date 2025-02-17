e5aa1e3bfb78f7734746c1432d4715e5464b7bb9
abelnega10 <abelnega10@gmail.com>
2025-02-17 18:56:12 +0100

    Worked on the banner

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Home from './pages/home/home.js'
 import Header from './components/header/header.js';
 import Footer from './components/footer/footer.js';
 import './App.css';
+import Banner from './components/Banner/Banner.js';
 
 function App() {
   return (
     <div className="App">
     
      <Header />
+     <Banner />
      <Footer/>
+
     </div>
   );
 }
